07dec5137a5ac93d842744513b09402a4af70921
BorisenkoPavel <PavelBorisenko@list.ru>
2018-10-26 02:06:15 +0700

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -31,18 +31,21 @@ $(function() {
 			var translate = "translate3d(" + Math.round(offsetX*offset) + "px," + Math.round(offsetY * offset) + "px, 0px";
 			$(el).css({
 				'transform':translate
-			})
-		})
-	})
-  
-  $(".fancybox").fancybox();
-  
+			});
+		});
+	});
+
+	$('[data-fancybox="gallery"]').fancybox({
+
+
+	});
+
 	$(document).ready(function(){
 		$(".navbar").on("click","a", function (event) {
 			event.preventDefault();
 			var id  = $(this).attr('href'),
 			top = $(id).offset().top;
-			$('body, html').animate({scrollTop: top}, 1000);
+			$('body, html').animate({scrollTop: top}, 350);
 		});
 	});
 
@@ -51,65 +54,51 @@ $(function() {
 			event.preventDefault();
 			var id  = $(this).attr('href'),
 			top = $(id).offset().top;
-			$('body, html').animate({scrollTop: top}, 1000);
+			$('body, html').animate({scrollTop: top}, 350);
 		});
 	});
 
 	var arr = {
-	an1: 'Собака',
-	an2: 'Кошка',
-	an3: 'Хомяк',
-	an4: 'Крыса',
-	an5: 'Черепаха',
-	an6: 'Рыбка',
-	an7: 'Попугай',
-	an8: 'Никого нет и не хочу',
-	an9: 'Другой ответ',
-};
-for (var elem in arr) {
-	document.getElementById(elem).addEventListener('click', inputHandler)
-};
-function inputHandler() {
-	document.getElementById("an-checked").innerHTML = arr[this.id];
-}
+		an1: 'Собака',
+		an2: 'Кошка',
+		an3: 'Хомяк',
+		an4: 'Крыса',
+		an5: 'Черепаха',
+		an6: 'Рыбка',
+		an7: 'Попугай',
+		an8: 'Никого нет и не хочу',
+		an9: 'Другой ответ',
+	};
+	for (var elem in arr) {
+		document.getElementById(elem).addEventListener('click', inputHandler);
+	}
+	function inputHandler() {
+		document.getElementById("an-checked").innerHTML = arr[this.id];
+	}
 
-		var sectionId = ['#ex1','#ex2','#ex3','#ex4','#ex5','#ex6']
-		var sectionPosition = sectionId.map(function(item, index, arr){
-			return $(item).offset().top;
+	var sectionId = ['#ex1','#ex2','#ex3','#ex4','#ex5','#ex6'];
+	var sectionPosition = sectionId.map(function(item, index, arr){
+		return $(item).offset().top-50;
+	});
+	var currentSection = 0;
+	$(document).ready(sectionScroll);
+	$(window).scroll(sectionScroll); 
+
+	function sectionScroll()
+	{
+		var winScrollTop = $(this).scrollTop();
+		var i = 0;
+		while (winScrollTop >= sectionPosition[i]) {
+			i++;
+		}
+		if (currentSection == i)
+			return ;
+		currentSection = i;
+		sectionId.forEach(function(item) {
+			$('a[href$="' + item + '"]').children('span').hide(350);
 		});
-		//console.log(sectionPosition)
-		$(window).scroll(function(){
-			//console.log($(this).scrollTop())
-			var winScrollTop = $(this).scrollTop();
-			var i = 0;
-			while (winScrollTop >= sectionPosition[i]) {
-				i++
-			}
-			sectionId.forEach(function(item) {
-				$('a[href$="' + item + '"]').children('span').hide();
-			})
-			var selector = 'a[href$="' + sectionId[i-1] + '"]'
-			$(selector).children('span').show()
-		 	console.log($(selector))
-			/*var winScrollTop = $(this).scrollTop();
-			if(winScrollTop > scrollToElem){
-	    //сработает когда пользователь доскроллит к элементу с классом .elem
-	    }*/
-	}); 
-	/*	var target = $('#ex?');
-		console.log(target)
-		var targetPos = target.offset().top;
-		var winHeight = $(window).height();
-		var scrollToElem = targetPos - winHeight;
-		$(window).scroll(function(){
-			var winScrollTop = $(this).scrollTop();
-			if(winScrollTop > scrollToElem){
-	    //сработает когда пользователь доскроллит к элементу с классом .elem
-	    }
-	 
-	}); */
- 
-
-
-
+		var selector = 'a[href$="' + sectionId[i-1] + '"]';
+		$(selector).children('span').slideToggle(350);
+	}
 });
+
